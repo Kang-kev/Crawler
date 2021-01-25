f2d80bfd08322351d220e6106d3a46e7b686bff5
woosin0113 <74348790+woosin0113@users.noreply.github.com>
2021-01-25 17:25:36 +0900

    Google Crawler(21.01.25)

---
+++ selenium/google.py
@@ -0,0 +1,50 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+#BEFORE START CRAWLING DOWNLOAD GOOGLEDRIVER.EXE FILE
+
+driver = webdriver.Chrome()
+nameList = [] # lIST OF THE NAME YOU WANT TO CRAWL
+for name in nameList:
+    driver.get("https://www.google.co.kr/imghp?hl=en-GB&tab=ri&ogbl")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys(name)
+    elem.send_keys(Keys.RETURN)
+    os.mkdir('D:/SMTM Project/' + name) #WHERE YOU WOULD LIKE TO SAVE THE DIRECTORIES
+
+
+# SCROLLING DOWN TILL THE 'MORE' BUTTON APPEARS */
+
+    SCROLL_PAUSE_TIME = 1
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+#SAVING IMAGES WITH NAME YOU WANT
+
+    images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+    count = 1
+    for image in images:
+        try:
+            image.click()
+            time.sleep(2)
+            imgUrl = driver.find_element_by_xpath(
+                '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+            urllib.request.urlretrieve(
+                imgUrl, "D:/SMTM Project/" + name + "/" + str(count) + ".jpg")
+            count = count + 1
+        except:
+            pass
+
+driver.close()
